1877229ccc06f850b07ec48d7d09e1dea9d5a60f
Frederik Vig <Frederik Vig>
2016-09-11 14:46:47 -0400

    Initial commit of boilerplate projects

---
+++ src/Geta.SocialChannels/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Geta.SocialChannels")]
+[assembly: AssemblyDescription("Geta Social Channels")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Geta")]
+[assembly: AssemblyProduct("Geta.SocialChannels")]
+[assembly: AssemblyCopyright("Copyright © Geta 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e56de5d-956c-4d8d-8a01-72867423377e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
