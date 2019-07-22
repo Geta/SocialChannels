77f577626594cd3a5e76e81ee2a49ed92fa94cfd
Marko Kovačina <m-kovacina@users.noreply.github.com>
2019-07-22 13:49:14 +0200

    Alloy sample site added
    Alloy sample site added as requested.

---
+++ samples/Geta.SocialChannels.Sample/Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace Geta.SocialChannels.Sample.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
