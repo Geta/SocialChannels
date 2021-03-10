3e489ff6e0a189c50423d8e47f0816674fa67fd2
Zbigniew Winiarski <zbigniew.winiarski@getadigital.com>
2021-03-10 10:49:10 +0100

    Removes Geta.SocialChannels.LinkedIn and Geta.SocialChannels.Facebook

--- src/Geta.SocialChannels.Facebook/FacebookFeedRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Geta.SocialChannels.Facebook
-{
-    public class FacebookFeedRequest
-    {
-        public string UserName { get; set; }
-        public int MaxCount { get; set; } = 10;
-    }
-}
--- src/Geta.SocialChannels.Facebook1/IFacebookService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Geta.SocialChannels.Facebook
-{
-    public interface IFacebookService
-    {
-        void Config(bool useCache, int cacheDurationInMinutes);
-        FacbookAuthorInformation GetInformation(string userName);
-        FacebookFeedResponse GetFacebookFeed(FacebookFeedRequest facebookFeedRequest);
-    }
-}
