9159f37d2f7575b59d1bef612f7ae8ec48f3518b
Zbigniew Winiarski <zbigniew.winiarski@getadigital.com>
2021-03-10 11:08:11 +0100

    Migrates Geta.SocialChannels to netstandard 2.0

--- src/Geta.SocialChannels.Twitter/TweetItemModel.cs
+++ src/Geta.SocialChannels.Twitter/TweetItemModel.cs
@@ -1,31 +1,11 @@
-﻿using System;
-using System.Globalization;
-
-namespace Geta.SocialChannels.Twitter
+﻿namespace Geta.SocialChannels.Twitter
 {
     public class TweetItemModel
     {
-        public string StatusId { get; set; }
-
         public string Text { get; set; }
 
         public string Link { get; set; }
 
         public string CreatedDate { get; set; }
-
-        public string CreatedTimeSince
-        {
-            get
-            {
-                DateTime dateTime;
-
-                if (!string.IsNullOrEmpty(CreatedDate) && DateTime.TryParseExact(CreatedDate, "ddd MMM dd HH:mm:ss zzzz yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime))
-                {
-                    return dateTime.ToTimeSinceString();
-                }
-
-                return string.Empty;
-            }
-        }
     }
 }
